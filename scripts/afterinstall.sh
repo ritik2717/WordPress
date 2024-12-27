177c949a1744a315d28138ee830e049330a64bf9
ritik2717 <tech.ritik22@gmail.com>
2024-12-27 15:41:29 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service httpd start
-chmod 777 /var/www/pipeline/*
-chown ec2-user:ec2-user /var/www/pipeline/*
+chmod 777 /var/www/html/demo.avertech.cloud/*
+chown ec2-user:ec2-user /var/www/html/demo.avertech.cloud/*
